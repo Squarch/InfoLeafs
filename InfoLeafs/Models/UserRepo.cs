c9a5822c48b0ed9307ab5d9003e39ad5d694ddd9
shigontsev <shigontsev1@mail.ru>
2016-04-05 18:11:26 +0300

    Трехслойка с Бд
    короче наакатах, потом если что обсудим какие еще методы нужны и какие
    зименить

---
+++ InfoLeafs/Models/UserRepo.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using Interfaces;
+//using Logic;
+
+namespace InfoLeafs.Models
+{
+    public class UserRepo
+    {
+        public static IUserLogic repo = new IUserLogic();
+        //static IUserLogic repo = new IUserLogic();
+
+    }
+}
---
+++ Interfaces/IUserLogic.cs
@@ -0,0 +1,47 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IUserLogic
+    {
+        #region not use
+        //User GetUser(int id);
+        //NewsFeedContent GetPostContent(int id);
+        //void Add(User user);
+        //bool AddAvatar(User user, string path);
+        //void AddPost(int id, NewsFeed post, NewsFeedContent content);
+        //bool Remove(int id);
+        //IEnumerable<User> GetAll();
+        //bool Update(User user);
+        #endregion
+        IQueryable<User> Users { get; }
+        IQueryable<NewsFeed> NewsFeeds { get; }
+        IQueryable<NewsFeedContent> NewsFeedContents { get; }
+        IQueryable<Subscription> Subscriptions { get; }
+        User GetUser(int id);
+        NewsFeedContent GetPostContent(int id);
+        Subscription GetSubscription(int id);
+        bool AddUser(User user);
+        bool AddAvatar(int id, string path);
+        bool AddPost(int id, NewsFeedContent content);
+        bool RemoveUser(int id);
+        bool RemovePost(int id);
+        bool UpdatePost(int id, string contentpost);
+        bool Subscribe(int idSubscriber, int idUser);
+        bool Unsubscribe(int id);
+
+
+
+        //bool CreateRole(Role instance);
+
+        //bool UpdateRole(Role instance);
+
+        //bool RemoveRole(int idRole);
+
+    }
+}
